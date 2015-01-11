05eb97eb214590d695dcc9313b1abc526661c356
Tomasz Janek <janek.tomasz@gmail.com>
2015-01-11 13:32:36 +0100

    Add build version comparator kata 01

---
+++ kata-01-build-version-comparator/src/main/groovy/BuildVersionComparator.groovy
@@ -0,0 +1,9 @@
+public class BuildVersionComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String firstVersion, String secondVersion) {
+        // TODO: Implement me
+        throw new RuntimeException()
+    }
+
+}
