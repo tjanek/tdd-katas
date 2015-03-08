2a51e579ea1db694f47931f5a9c09c1484de64f1
Tomasz Janek <janek.tomasz@gmail.com>
2015-03-08 13:28:16 +0100

    Tic Tac Toe Kata specifications

---
+++ kata-11-tic-tac-toe/src/main/groovy/TicTacToe.groovy
@@ -0,0 +1,15 @@
+
+class TicTacToe {
+
+    def markAsCircleAt(int x, int y) {
+
+    }
+
+    def markedAsCircle(int x, int y) {
+
+    }
+
+    boolean markedAsCross(int x, int y) {
+
+    }
+}
---
+++ kata-11-tic-tac-toe/src/test/groovy/TicTacToeSpec.groovy
@@ -0,0 +1,28 @@
+import spock.lang.Specification
+
+class TicTacToeSpec extends Specification {
+
+    TicTacToe ticTacToe = new TicTacToe()
+
+    def "should mark fields as circle"() {
+        when:
+        ticTacToe.markAsCircleAt(x, y)
+
+        then:
+        ticTacToe.markedAsCircle(x, y) == true
+        ticTacToe.markedAsCross(x, y) == false
+
+
+        where:
+        x | y
+        0 | 0
+        0 | 1
+        0 | 2
+        1 | 0
+        1 | 1
+        1 | 2
+        2 | 0
+        2 | 1
+        2 | 2
+    }
+}
