9e46b109f119a1bc9e7b271a0773e0db3f3cd560
Tomasz Janek <janek.tomasz@gmail.com>
2015-03-14 07:51:34 +0100

    Specification for all fields must be not marked at the beginning

--- kata-11-tic-tac-toe/src/test/groovy/TicTacToeSpec.groovy
+++ kata-11-tic-tac-toe/src/test/groovy/TicTacToeSpec.groovy
@@ -47,4 +47,22 @@ class TicTacToeSpec extends Specification {
         2 | 1
         2 | 2
     }
+
+    def "should all fields must be not marked at the beginning"() {
+        expect:
+        ticTacToe.markedAsCross(x, y) == false
+        ticTacToe.markedAsCircle(x, y) == false
+
+        where:
+        x | y
+        0 | 0
+        0 | 1
+        0 | 2
+        1 | 0
+        1 | 1
+        1 | 2
+        2 | 0
+        2 | 1
+        2 | 2
+    }
 }
